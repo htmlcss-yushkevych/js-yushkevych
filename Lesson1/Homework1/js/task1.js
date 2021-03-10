ca65f14cd436e7723568a09fd6a157542cdfa355
htmlcss-yushkevych <vikusha736@gmail.com>
2021-03-10 22:49:17 +0200

    Update task1.js

--- Lesson1/Homework1/js/task1.js
+++ Lesson1/Homework1/js/task1.js
@@ -7,8 +7,10 @@ var k = (y1 - y2) / (x1 - x2);
 var b = k * x2;
 b = y2 - b;
 document.write("y = " + k + "x+" + b + '<br>');
-var answer1 = confirm("Рассчитать результат?")
-if (answer1 == true){
-var x = prompt("Введите значене х", 1);
-var y = k * x + b;
-document.write("у = ", y + '<br>');}
+
+var response = confirm("Рассчитать результат?")
+if (response == true) {
+    var x = prompt("Введите значение х", 1);
+    var y = k * x + b;
+    document.write("у = ", y + '<br>');
+}
